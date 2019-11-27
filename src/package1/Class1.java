1d1415c7483a23beb9a91564e0e3c783a458b3bf
goelsri <srishtigoel07@gmail.com>
2019-11-28 01:45:57 +0530

    phle bar h bhai

---
+++ src/package1/Class1.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class Class1 {
+
+	
+	public void method1()
+	{
+		System.out.println("this is my first git project");
+	}
+	
+	public void method2()
+	{
+		System.out.println("adding some random data");
+	}
+}
---
+++ src/package1/ClassMain.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class ClassMain {
+
+	
+	public static void main(String A[])
+	{
+		Class1 obj = new Class1();
+			obj.method1();
+			obj.method2();
+	}
+}
